16d6a1d448483e1dd52c86fdbe0bf038704944e2
sunteya <sunteya@gmail.com>
2017-10-17 15:29:17 +0800

    gost: upgrade to 2.4

--- gost/build.sh
+++ gost/build.sh
@@ -2,13 +2,14 @@ set -x
 set -e
 
 version=$1
+revision=$2
 
 apk add wget ca-certificates --update
-wget -O - https://github.com/ginuerzh/gost/releases/download/v${version}/gost_${version}_linux_amd64.tar.gz | tar xvz
+wget -O - https://github.com/ginuerzh/gost/releases/download/v${version}/gost_${revision}_linux_amd64.tar.gz | tar xvz
 
 mkdir -p /app
-mv /gost_${version}_linux_amd64/gost /app/gost
-rm -rf /gost_${version}_linux_amd64
+mv /gost_${revision}_linux_amd64/gost /app/gost
+rm -rf /gost_${revision}_linux_amd64
 
 apk del wget ca-certificates --purge
 rm -rf /var/cahce/apk/*
