5d14258803b1376881b8e127300c58348fdd38aa
sunteya <sunteya@gmail.com>
2018-03-25 23:11:53 +0800

    gost: upgrade to 2.5-rc1

--- gost/build.sh
+++ gost/build.sh
@@ -1,15 +1,14 @@
-set -x
-set -e
+set -ex
 
 version=$1
 revision=$2
 
-apk add wget ca-certificates --update
+apk --no-cache --virtual .build-deps add wget ca-certificates
 wget -O - https://github.com/ginuerzh/gost/releases/download/v${version}/gost_${revision}_linux_amd64.tar.gz | tar xvz
 
 mkdir -p /app
 mv /gost_${revision}_linux_amd64/gost /app/gost
 rm -rf /gost_${revision}_linux_amd64
 
-apk del wget ca-certificates --purge
-rm -rf /var/cahce/apk/*
+apk del .build-deps
+rm -rf /tmp/*
