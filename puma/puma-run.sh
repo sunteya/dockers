6925e150be8e3cf7bb3d76184a5d3da524adfff5
sunteya <sunteya@gmail.com>
2018-05-17 09:25:12 +0800

    puma: puma support pumactl state

--- puma/puma-run.sh
+++ puma/puma-run.sh
@@ -2,4 +2,7 @@
 
 cd $APP_CURRENT_PATH
 exec 2>&1
-exec chpst -u app bundle exec puma -C config/puma.rb --environment production
+exec chpst -u app bundle exec puma --config config/puma.rb \
+                                   --environment production \
+                                   --control "unix://$PWD/tmp/pids/pumactl.sock" \
+                                   --state $PWD/tmp/pids/puma.state
