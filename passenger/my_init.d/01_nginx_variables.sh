78d85d9dda2d8451d378ad45541c22b175aff38f
sunteya <sunteya@gmail.com>
2026-04-01 18:18:02 +0800

    passenger: render nginx sites from templates

--- passenger/my_init.d/01_nginx_variables.sh
+++ passenger/my_init.d/01_nginx_variables.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "set \$app_current_path '$APP_CURRENT_PATH';" > /etc/nginx/snippets/variables.conf
+render_site_template() {
+	local template_file="$1"
+	local output_file="/etc/nginx/sites-available/$(basename "$template_file")"
+	local template_content
+
+	template_content="$(< "$template_file")"
+	template_content="${template_content//\$\{APP_CURRENT_PATH\}/${APP_CURRENT_PATH%/}}"
+	template_content="${template_content//\$\{RAILS_RELATIVE_URL_ROOT\}/${RAILS_RELATIVE_URL_ROOT%/}}"
+	printf '%s\n' "$template_content" > "$output_file"
+}
+
+for template_file in /etc/nginx/sites-templates/*; do
+	[ -f "$template_file" ] || continue
+	render_site_template "$template_file"
+done
